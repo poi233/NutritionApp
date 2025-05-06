298b96734ba1404df11f2526962fe74fd6425185
Yihao Pu <puyihao1012@gmail.com>
2025-05-06 06:33:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <tr>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <RedirectErrorBoundary router={{...}}>
    >       <InnerLayoutRouter url="/" tree={[...]} cacheNode={{lazyData:null, ...}} segmentPath={[...]}>
    >         <ClientPageRoot Component={function Home} searchParams={{}} params={{}}>
    >           <Home params={Promise} searchParams={Promise}>
    >             <main className="container ...">
    >               <div>
    >               <div className="w-full max...">
    >                 <WeeklyPlanner recipes={[...]} onDeleteRecipe={function handleDeleteRecipe} daysOfWeek={[...]} ...>
    >                   <_c className="border sha...">
    >                     <div className="relative w...">
    >                       <table ref={null} className="w-full cap...">
    >                         <_c2>
    >                           <thead ref={null} className={"[&_tr]:b..."}>
    >                             <_c8 className="bg-muted/5...">
    > >                             <tr
    > >                               ref={null}
    > >                               className="border-b transition-colors data-[state=selected]:bg-muted bg-muted/50 hover..."
    > >                             >
    >                                 <_c10>
    > >                               {" "}
    >                                 ...
    >                         ...
    >                 ...
    >         ...
    >
    >
    > src/components/ui/table.tsx (59:5) @ _c8
    >
    >   57 | >(({ className, ...props }, ref) => {
    >   58 |   return (
    > > 59 |     <tr
    >      |     ^
    >   60 |       ref={ref}
    >   61 |       className={cn(
    >   62 |         "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
    >
    > Call Stack
    > 24
    >
    > Show 20 ignore-listed frame(s)
    > tr
    > <anonymous> (0:0)
    > _c8
    > src/components/ui/table.tsx (59:5)
    > WeeklyPlanner
    > src/components/weekly-planner.tsx (64:9)
    > Home
    > src/app/page.tsx (609:13)

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -54,18 +54,16 @@ TableFooter.displayName = "TableFooter"
 const TableRow = React.forwardRef<
   HTMLTableRowElement,
   React.HTMLAttributes<HTMLTableRowElement>
->(({ className, ...props }, ref) => {
-  return (
-    <tr
-      ref={ref}
-      className={cn(
-        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
-        className
-      )}
-      {...props}
-    />
-  );
-})
+>(({ className, ...props }, ref) => (
+  <tr
+    ref={ref}
+    className={cn(
+      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      className
+    )}
+    {...props}
+  />
+))
 TableRow.displayName = "TableRow"
 
 const TableHead = React.forwardRef<
@@ -97,7 +95,7 @@ TableCell.displayName = "TableCell"
 
 const TableCaption = React.forwardRef<
   HTMLTableCaptionElement,
-  React.HTMLAttributes<HTMLTableCaptionElement>
+  React.HTMLAttributes<HTMLCaptionElement>
 >(({ className, ...props }, ref) => (
   <caption
     ref={ref}
